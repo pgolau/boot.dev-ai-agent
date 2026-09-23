62d3741db53952dca8b0c7f6b1f8c57405f53fce
pgolau <327836928+pgolau@users.noreply.github.com>
2026-09-23 21:29:22 +0200

    CH1 L3 OpenRouter API

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError(
+            "OPENROUTER_API_KEY environment variable was not found. "
+            "Please set it in your environment or .env file."
+        )
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
