e66b56aec3473689938fb0b4eae3df79029e82c5
pgolau <327836928+pgolau@users.noreply.github.com>
2026-09-26 16:08:57 +0200

    CH2 L2 Validate Paths

---
+++ test_get_files_info.py
@@ -0,0 +1,11 @@
+from functions.get_files_info import get_files_info
+
+
+def test_get_files_info():
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "../"))
+    print(get_files_info("calculator", "main.py"))
+
+if __name__ == "__main__":
+    test_get_files_info()
